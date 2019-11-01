717a44b4e3840887a66f779bda599fa199afbd23
Abhishek Mishra <amishra6@buffalo.edu>
2019-11-01 11:51:23 -0400

    box_mesh example

---
+++ src/dm/impls/plex/examples/tutorials/ex_box_mesh.c
@@ -0,0 +1,35 @@
+static char help[] = "Creating a box mesh\n\n";
+
+#include <petscdmplex.h>
+
+int main(int argc, char **argv)
+{
+  DM             dm, dmDist = NULL;
+  Vec            u;
+  PetscViewer    viewer;
+  PetscInt	 dim = 2;
+  PetscBool      interpolate = PETSC_TRUE;
+  PetscErrorCode ierr;
+
+  ierr = PetscInitialize(&argc, &argv, NULL,help);if (ierr) return ierr;
+  ierr = PetscOptionsGetInt(NULL,NULL, "-dim", &dim, NULL);CHKERRQ(ierr);
+  /* Create a mesh */
+  ierr = DMPlexCreateBoxMesh(PETSC_COMM_WORLD, dim, PETSC_FALSE, NULL, NULL, NULL, NULL, interpolate, &dm);CHKERRQ(ierr);
+  ierr = DMPlexDistribute(dm, 0, NULL, &dmDist);CHKERRQ(ierr);
+  if (dmDist) {ierr = DMDestroy(&dm);CHKERRQ(ierr); dm = dmDist;}
+  /* Create a Vec with this layout and view it */
+  ierr = DMGetGlobalVector(dm, &u);CHKERRQ(ierr);
+  ierr = PetscViewerCreate(PETSC_COMM_WORLD, &viewer);CHKERRQ(ierr);
+//  ierr = PetscViewerSetType(viewer, PETSCVIEWERASCII);CHKERRQ(ierr);
+//  ierr = PetscViewerPushFormat(viewer, PETSC_VIEWER_DEFAULT);CHKERRQ(ierr);
+  ierr = VecView(u, viewer);CHKERRQ(ierr);
+  ierr = DMViewFromOptions(dm, NULL, "-dm_view");CHKERRQ(ierr);
+  ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
+  ierr = DMRestoreGlobalVector(dm, &u);CHKERRQ(ierr);
+
+  /* Cleanup */
+  ierr = DMDestroy(&dm);CHKERRQ(ierr);
+  ierr = PetscFinalize();
+  return ierr;
+}
+
