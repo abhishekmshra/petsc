8e7d7aeb35983d0282a20e463b74ac069a1b7139
Abhishek Mishra <amishra6@buffalo.edu>
2019-12-20 11:57:09 -0500

    create base dmplex, refine it, convert it to dm_forest, use dm adpat to refine cells and convert back to dmplex

--- src/dm/impls/plex/examples/tutorials/ex_forest.c
+++ src/dm/impls/plex/examples/tutorials/ex_forest.c
@@ -8,7 +8,7 @@ static char help[] = "Creating a box mesh and refining\n\n";
 
 int main(int argc, char **argv)
 {
-  DM             dm, dmf, dmp;
+  DM             dm, base, preforest, postforest;
   Vec            u;
   PetscViewer    viewer;
   PetscInt	 nRefine, dim = 2;
@@ -18,29 +18,42 @@ int main(int argc, char **argv)
 
   ierr = PetscInitialize(&argc, &argv, NULL,help);if (ierr) return ierr;
   ierr = PetscOptionsGetInt(NULL,NULL, "-dim", &dim, NULL);CHKERRQ(ierr);
-  /* Create a mesh */
-  ierr = DMCreate(PETSC_COMM_WORLD, &dm);CHKERRQ(ierr);
-  ierr = DMSetType(dm, DMFOREST);CHKERRQ(ierr);
-  ierr = DMPlexCreateBoxMesh(PETSC_COMM_WORLD, dim, PETSC_FALSE, NULL, NULL, NULL, NULL, interpolate, &dmp);CHKERRQ(ierr);
-  ierr = DMForestSetBaseDM(dm, dmp);CHKERRQ(ierr);
-  ierr = DMForestTemplate(dm, PETSC_COMM_WORLD, &dmf);
+  /* Create a base mesh */
+  ierr = DMPlexCreateBoxMesh(PETSC_COMM_WORLD, dim, PETSC_FALSE, NULL, NULL, NULL, NULL, interpolate, &base);CHKERRQ(ierr);
+  ierr = DMRefine(base, PETSC_COMM_WORLD, &base);CHKERRQ(ierr);
+  ierr = DMGetDimension(base,&dim);CHKERRQ(ierr);
+
+  ierr = DMCreate(PETSC_COMM_WORLD, &preforest);CHKERRQ(ierr);
+  ierr = DMSetType(preforest, (dim == 2) ? DMP4EST : DMP8EST);CHKERRQ(ierr);
+  ierr = DMCopyDisc(base,preforest);CHKERRQ(ierr);
+  ierr = DMForestSetBaseDM(preforest, base);CHKERRQ(ierr);
+  ierr = DMSetUp(preforest);CHKERRQ(ierr);
+  ierr = DMDestroy(&base);CHKERRQ(ierr);
+//  ierr = DMForestTemplate(dm, PETSC_COMM_WORLD, &dmf);
 
   /* Create a Vec with this layout and view it */
   ierr = DMLabelCreate(PETSC_COMM_SELF,"adapt",&adaptLabel);CHKERRQ(ierr);
-  ierr = DMAddLabel(dm,adaptLabel);CHKERRQ(ierr);
+
   ierr = DMLabelSetValue(adaptLabel,1,DM_ADAPT_REFINE);CHKERRQ(ierr);
+  ierr = DMLabelSetValue(adaptLabel,15,DM_ADAPT_REFINE);CHKERRQ(ierr);
 //  ierr = DMLabelGetValue(adaptLabel,1,&value);CHKERRQ(ierr);
 //  ierr = DMAdaptLabel(dm, adaptLabel, &dm);CHKERRQ(ierr);
   //ierr = DMRefine(dm, PETSC_COMM_WORLD, &dm);CHKERRQ(ierr);
 //  ierr = DMGetGlobalVector(dm, &u);CHKERRQ(ierr);
-  ierr = DMForestSetAdaptivityLabel(dmf,adaptLabel);CHKERRQ(ierr);
-  ierr = DMSetUp(dmf);CHKERRQ(ierr);
+
+  ierr = DMForestTemplate(preforest, PETSC_COMM_WORLD, &postforest);CHKERRQ(ierr);
+  ierr = DMDestroy(&preforest);CHKERRQ(ierr);
+  ierr = DMForestSetAdaptivityLabel(postforest,adaptLabel);CHKERRQ(ierr);
+  ierr = DMSetUp(postforest);CHKERRQ(ierr);
+  ierr = DMForestSetAdaptivityLabel(postforest,adaptLabel);CHKERRQ(ierr);
+  ierr = DMSetUp(postforest);CHKERRQ(ierr);
 
 
 //  ierr = DMAdaptLabel(dm, adaptLabel, &dm);CHKERRQ(ierr);
 
 
-//  ierr = DMConvert(dmf, DMPLEX, &dm);CHKERRQ(ierr);
+  ierr = DMConvert(postforest, DMPLEX, &dm);CHKERRQ(ierr);
+  ierr = DMDestroy(&postforest);CHKERRQ(ierr);
   ierr = PetscViewerCreate(PETSC_COMM_WORLD, &viewer);CHKERRQ(ierr);
 //  ierr = VecView(u, viewer);CHKERRQ(ierr);
 //  ierr = PetscPrintf(PETSC_COMM_WORLD, "nRefine: %D",nRefine);CHKERRQ(ierr);
