37b2c54e9c9303be19afa168488b9431f04279dd
Abhishek Mishra <amishra6@buffalo.edu>
2020-03-15 16:40:07 -0400

    Updated the refinement func and code cleanup

--- src/dm/impls/plex/examples/tutorials/ex_circle_adapt.c
+++ src/dm/impls/plex/examples/tutorials/ex_circle_adapt.c
@@ -7,27 +7,35 @@ static char help[] = "Adaptively refine a box mesh on the boundary of a circle\n
 #include <petscds.h>
 #include <petscmath.h>
 
-static PetscErrorCode AdaptiveCircumferenceRefinement(DM cdm, Vec coordinates, PetscInt p, PetscInt xcenter, PetscInt ycenter, PetscInt cstart, PetscInt cend, DMLabel *adaptLabel)
+static PetscErrorCode AdaptiveCircumferenceRefinement(DM forest, PetscInt p, PetscInt xcenter, PetscInt ycenter, DMLabel *adaptLabel)
 {
-  PetscInt c;
+  DM             cdm;
+  Vec            coordinates;
+  PetscInt       c, cstart, cend;
   PetscErrorCode ierr;
 
   PetscFunctionBeginUser;
+
+  ierr = DMForestGetCellChart(forest, &cstart, &cend);CHKERRQ(ierr);
+  ierr = DMGetCoordinateDM(forest, &cdm);CHKERRQ(ierr);
+  ierr = DMGetCoordinatesLocal(forest, &coordinates);CHKERRQ(ierr);
+  ierr = DMLabelCreate(PETSC_COMM_SELF,"adapt",adaptLabel);CHKERRQ(ierr);
+
   for (c = cstart; c < cend; ++c)
     {
-      PetscInt       csize;
-      PetscScalar    *coords = NULL;
-      PetscReal	     min, x1, x2, x3, x4, y1, y2, y3, y4, diag, S1, S2, S3, S4;
+      PetscInt    csize;
+      PetscScalar *coords = NULL;
+      PetscReal	  min, x1, x2, x3, x4, y1, y2, y3, y4, diag, S1, S2, S3, S4;
 
+      /* Get Coordinates */
       ierr = DMPlexVecGetClosure(cdm, NULL, coordinates, c, &csize, &coords);CHKERRQ(ierr);
-
       x1 = PetscRealPart(coords[0]); x2 = PetscRealPart(coords[2]);
       x3 = PetscRealPart(coords[4]); x4 = PetscRealPart(coords[6]);
       y1 = PetscRealPart(coords[1]); y2 = PetscRealPart(coords[3]);
       y3 = PetscRealPart(coords[5]); y4 = PetscRealPart(coords[7]);
 
       diag = PetscSqrtReal(PetscSqr(x3-x1) + PetscSqr(y3-y1));
-
+      /* SDF at each cell vertex */
       S1 = PetscAbsReal(PetscSqrtReal(PetscSqr(xcenter-x1) + PetscSqr(ycenter-y1)) - p);
       S2 = PetscAbsReal(PetscSqrtReal(PetscSqr(xcenter-x2) + PetscSqr(ycenter-y2)) - p);
       S3 = PetscAbsReal(PetscSqrtReal(PetscSqr(xcenter-x3) + PetscSqr(ycenter-y3)) - p);
@@ -35,14 +43,8 @@ static PetscErrorCode AdaptiveCircumferenceRefinement(DM cdm, Vec coordinates, P
 
       min = PetscMin(PetscMin(S1, S2), PetscMin(S3, S4));
 
-      if (min<diag)
-      {
-        ierr = DMLabelSetValue(*adaptLabel,c,DM_ADAPT_REFINE);CHKERRQ(ierr);
-      }
-      else
-      {
-        ierr = DMLabelSetValue(*adaptLabel,c,DM_ADAPT_KEEP);CHKERRQ(ierr);
-      }
+      if (min<diag) {ierr = DMLabelSetValue(*adaptLabel,c,DM_ADAPT_REFINE);CHKERRQ(ierr);}
+      else {ierr = DMLabelSetValue(*adaptLabel,c,DM_ADAPT_KEEP);CHKERRQ(ierr);}
 
       ierr = DMPlexVecRestoreClosure(cdm, NULL, coordinates, c, &csize, &coords);CHKERRQ(ierr);
     }
@@ -51,56 +53,54 @@ static PetscErrorCode AdaptiveCircumferenceRefinement(DM cdm, Vec coordinates, P
 
 int main(int argc, char **argv)
 {
-  DM             dm, cdm, base, preforest, postforest;
-  Vec            coordinates;
-  PetscInt	 dim = 2, cstart, cend, n, xcenter, ycenter, p = 1, nrefine = 4;
+  DM             dm, base, preforest, postforest;
+  PetscInt	 dim = 2, n, xcenter, ycenter, p = 1, nrefine = 4;
   PetscReal      lower[2] = {0,0}, upper[2] = {4,4};
   PetscBool      interpolate = PETSC_TRUE;
   DMLabel        adaptLabel = NULL;
   PetscErrorCode ierr;
 
   ierr = PetscInitialize(&argc, &argv, NULL,help);if (ierr) return ierr;
+  /* Dimension of mesh */
   ierr = PetscOptionsGetInt(NULL,NULL, "-dim", &dim, NULL);CHKERRQ(ierr);
+  /* Refinement Level */
   ierr = PetscOptionsGetInt(NULL,NULL, "-nrefine", &nrefine, NULL);CHKERRQ(ierr);
+  /* Radius of circle */
+  ierr = PetscOptionsGetInt(NULL,NULL, "-p", &p, NULL);CHKERRQ(ierr);
+  /* Calculate center coordinates */
+  xcenter = (upper[0]+lower[0])/2; ycenter = (upper[1]+lower[1])/2;
 
-  xcenter = (upper[0]+lower[0])/2;
-  ycenter = (upper[1]+lower[1])/2;
-
-  /* Create a base mesh */
+  /* Create a base DMPlex mesh */
   ierr = DMPlexCreateBoxMesh(PETSC_COMM_WORLD, dim, PETSC_FALSE, NULL, lower, upper, NULL, interpolate, &base);CHKERRQ(ierr);
-
   ierr = DMGetDimension(base,&dim);CHKERRQ(ierr);
-
+  /* Covert Plex mesh to Forest and destroy base */
   ierr = DMCreate(PETSC_COMM_WORLD, &preforest);CHKERRQ(ierr);
   ierr = DMSetType(preforest, (dim == 2) ? DMP4EST : DMP8EST);CHKERRQ(ierr);
   ierr = DMForestSetBaseDM(preforest, base);CHKERRQ(ierr);
   ierr = DMSetUp(preforest);CHKERRQ(ierr);
   ierr = DMDestroy(&base);CHKERRQ(ierr);
 
-
+  /* Refinement Loop */
   for (n = 0; n < nrefine; ++n)
   {
+    /* Call Refinement Function */
+    ierr = AdaptiveCircumferenceRefinement(preforest, p, xcenter, ycenter, &adaptLabel);CHKERRQ(ierr);
 
-    ierr = DMForestGetCellChart(preforest, &cstart, &cend);CHKERRQ(ierr);
-    ierr = DMGetCoordinateDM(preforest, &cdm);CHKERRQ(ierr);
-    ierr = DMGetCoordinatesLocal(preforest, &coordinates);CHKERRQ(ierr);
-    ierr = DMLabelCreate(PETSC_COMM_SELF,"adapt",&adaptLabel);CHKERRQ(ierr);
-
-    ierr = AdaptiveCircumferenceRefinement(cdm, coordinates, p, xcenter, ycenter, cstart, cend, &adaptLabel);CHKERRQ(ierr);
+    /* Apply adaptLabel to the forest and set up */
     ierr = DMForestTemplate(preforest, PETSC_COMM_WORLD, &postforest);CHKERRQ(ierr);
     ierr = DMDestroy(&preforest);CHKERRQ(ierr);
     ierr = DMForestSetAdaptivityLabel(postforest,adaptLabel);CHKERRQ(ierr);
     ierr = DMSetUp(postforest);CHKERRQ(ierr);
-
     ierr = DMForestTemplate(postforest, PETSC_COMM_WORLD, &preforest);CHKERRQ(ierr);
     ierr = DMDestroy(&postforest);CHKERRQ(ierr);
     ierr = DMSetUp(preforest);CHKERRQ(ierr);
+    /* Rest Label */
     ierr = DMLabelReset(adaptLabel);CHKERRQ(ierr);
   }
-
+  /* Convert Forest back to Plex for visualization */
   ierr = DMConvert(preforest, DMPLEX, &dm);CHKERRQ(ierr);
   ierr = DMDestroy(&preforest);CHKERRQ(ierr);
-
+  /* DM View */
   ierr = DMViewFromOptions(dm, NULL, "-dm_view");CHKERRQ(ierr);
 
   /* Vector View*/
